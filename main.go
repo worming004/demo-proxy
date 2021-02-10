f05d75d66b00f674a51de6d1f14b22b9e535585f
Mathieu Scolas <worming@hotmail.be>
2021-02-10 23:10:56 +0100

    adapt for k8s

--- main.go
+++ main.go
@@ -8,13 +8,19 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/proxy", repeatHandler)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+	mux.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
 func repeatHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("/proxy")
 	client := http.DefaultClient
-	res, err := client.Get("http://demo-endpoint:9090/demo-endpoint/text")
+	res, err := client.Get("http://demoendpoint:9090/demo-endpoint/text")
 	if err != nil {
 		panic(err)
 	}
